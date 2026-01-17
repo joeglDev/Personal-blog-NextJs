60e22047038cc7be8842b96c0132328a033297fc
Joe Gilbert (He/Him) <104070284+joeglDev@users.noreply.github.com>
2026-01-17 20:41:28 +0000

    Feat/add-progress-flag-to-footer  (#5)

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
-import {faEnvelope, faFile} from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faFile } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Footer.module.css";
 
 export const Footer = () => (
@@ -32,13 +32,20 @@ export const Footer = () => (
       <p>Github</p>
     </a>
 
-      <a
-          href="https://joe-gilbert-developer.xyz"
-          target="_blank"
-          className={styles.iconContainer}
-      >
-          <FontAwesomeIcon className={styles.icon} icon={faFile} />
-          <p>My cv</p>
-      </a>
+    <a
+      href="https://joe-gilbert-developer.xyz"
+      target="_blank"
+      className={styles.iconContainer}
+    >
+      <FontAwesomeIcon className={styles.icon} icon={faFile} />
+      <p>My cv</p>
+    </a>
+
+    <a
+      className={`${styles.progress} ${styles.flag} ${styles.iconContainer}`}
+      href="https://goodlawproject.org/resource/support-resources-for-trans/"
+      target="_blank"
+      aria-label="Progress Pride Flag. The traditional gay pride flag with 6 bars: red, orange, yellow, green, blue, and indigo. On the left side, it has triangles of color: white, pink, light blue, brown, and black. The brown stripe represents people of colour, black represents people sadly lost through the HIV / AIDs crisis or are suffering with HIV / AIDs today and the white, blue and pink represents the transgender community."
+    ></a>
   </footer>
 );

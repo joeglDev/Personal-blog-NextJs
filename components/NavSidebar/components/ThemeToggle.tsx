554f5848870f9d330f3c0d31e6ebf05a18c8ff5a
hiroji <joegilbertdev@outlook.com>
2026-02-02 14:37:12 +0000

    add warning that light mode is experimental and update table

--- components/NavSidebar/components/ThemeToggle.tsx
+++ components/NavSidebar/components/ThemeToggle.tsx
@@ -20,21 +20,20 @@ export const ThemeToggle = () => {
   }, [isChecked]);
 
   return (
-      <div className={styles.flexColumn}>
-          <div className={styles.themeContainer}>
-              <label className={styles.label} htmlFor="light-dark-mode-toggle">
-                  <span>Current theme:</span>
-                  <FontAwesomeIcon icon={isChecked ? faMoon : faSun} />
-              </label>
-              <Switch
-                  aria-label="Set light / dark theme"
-                  id="light-dark-mode-toggle"
-                  checked={isChecked}
-                  onClick={onClick}
-              />
-          </div>
-          {!isChecked && <p>⚠️: Please note that light mode is experimental</p>}
+    <div className={styles.flexColumn}>
+      <div className={styles.themeContainer}>
+        <label className={styles.label} htmlFor="light-dark-mode-toggle">
+          <span>{`${isChecked ? "Dark" : "Light"} theme `}</span>
+          <FontAwesomeIcon icon={isChecked ? faMoon : faSun} />
+        </label>
+        <Switch
+          aria-label="Set light / dark theme"
+          id="light-dark-mode-toggle"
+          checked={isChecked}
+          onClick={onClick}
+        />
       </div>
-
+      {!isChecked && <p>⚠️: Please note that light mode is experimental</p>}
+    </div>
   );
 };

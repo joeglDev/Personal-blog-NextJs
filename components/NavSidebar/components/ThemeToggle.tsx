68de869f888dd683db8ecab5545fecbc13566df5
Joe Gilbert (He/Him) <104070284+joeglDev@users.noreply.github.com>
2026-02-03 13:05:35 +0000

    Fix: Colour theme reverts  (#10)
    Fix: Use next theme provider context to set theme more durably.
    
    Closes: #9

--- components/NavSidebar/components/ThemeToggle.tsx
+++ components/NavSidebar/components/ThemeToggle.tsx
@@ -1,23 +1,28 @@
 "use client";
 
 import { Switch } from "@/components/ui/switch";
-import { useEffect, useState } from "react";
 import styles from "./ThemeToggle.module.css";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export const ThemeToggle = () => {
-  const [isChecked, setIsChecked] = useState(true);
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-  const onClick = () => setIsChecked((prevState) => !prevState);
+  const isChecked = theme !== "light";
+  const onClick = () => (isChecked ? setTheme("light") : setTheme("dark"));
 
+  // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
-    if (isChecked) {
-      document.body.setAttribute("data-theme", "dark");
-    } else {
-      document.body.setAttribute("data-theme", "light");
-    }
-  }, [isChecked]);
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <div className={styles.flexColumn}>
